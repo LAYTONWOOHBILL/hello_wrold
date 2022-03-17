c4d832e07a2b4e512cce50d702cb84d73534574c
LAYTONWOOHBILL <wilson833412@gmail.com>
2022-03-17 13:50:43 -0500

    parent widget that stores mutable state

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ of which the following are commonly used:
   A Container can also have margins, padding, and constraints applied to its size.
   In addition, a Container can be transformed in three dimensional space using a matrix.
 */
+
+/*
 void main() {
   runApp(
     const MaterialApp(
@@ -33,6 +35,7 @@ void main() {
     ),
   );
 }
+*/
 
 
 /*
@@ -307,4 +310,149 @@ class _CounterState extends State<Counter> {
         ],
     );
   }
+}
+
+
+// **********************************
+// **  Bringing it all together    **
+// **********************************
+
+/*
+* When the parent receives the onCartChanged callback,
+* the parent updates its internal state,
+* which triggers the parent to rebuild and
+* create a new instance of ShoppingListItem with the new inCart value.
+* Although the parent creates a new instance of ShoppingListItem when it rebuilds,
+* that operation is cheap because the framework compares the newly built widgets
+* with the previously built widgets and
+* applies only the differences to the underlying RenderObject.
+* */
+
+
+class Product{
+  const Product({required this.name});
+  final String name;
+}
+
+typedef CartChangeCallback = Function(Product product, bool inCart);
+
+// The ShoppingListItem widget follows a common pattern for stateless widgets
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({
+    required this.product,
+    required this.inCart,
+    required this.onCartChanged,
+  }) : super(key: ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangeCallback onCartChanged;
+
+  Color _getColor(BuildContext context){
+    // The theme depends on the BuildContext because different
+    // parts of the tree can have different themes.
+    // The BuildContext indicates where the build is
+    // taking place and therefore which theme to use.
+
+    return inCart //
+        ? Colors.black54
+        : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context){
+    if (!inCart) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: (){
+        onCartChanged(product,inCart);
+        // the inCart boolean toggles between two visual appearances
+        // the widget calls the onCartChanged function it received from its parent widget.
+        // This pattern lets you store state higher in the widget hierarchy,
+        // which causes the state to persist for longer periods of time
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(product.name, style: _getTextStyle(context)),
+    );
+  }
+}
+
+// The ShoppingList class extends StatefulWidget, 
+// which means this widget stores mutable state.
+
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({required this.products ,Key? key}) : super(key: key);
+  final List<Product> products;
+
+  // The framework calls createState the first time
+  // a widget appears at a given location in the tree.
+  // If the parent rebuilds and uses the same type of
+  // widget (with the same key), the framework re-uses
+  // the State object instead of creating a new State object.
+
+  @override
+  State<ShoppingList> createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart){
+    setState(() {
+      // When a user changes what's in the cart, you need
+      // to change _shoppingCart inside a setState call to
+      // trigger a rebuild.
+      // The framework then calls build, below,
+      // which updates the visual appearance of the app.
+      if (!inCart){
+        _shoppingCart.add(product);
+      }else{
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Shopping List'),),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );}
+        ).toList(),
+      ),
+    );
+  }
+}
+
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: ShoppingList(
+        products: [
+          Product(name: 'Eggs'),
+          Product(name: 'Flour'),
+          Product(name: 'Chocolate chips'),
+        ],
+      ),
+    ),
+  );
 }
